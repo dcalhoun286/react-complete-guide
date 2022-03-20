f9fab26d6eaf49dd93fecffa42c2537fba87f2d9
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-03-20 15:15:19 -0700

    added event handler on expense year filter

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,11 +1,20 @@
+// import { useState } from 'react';
+
 import './ExpenseFilter.css';
 
 const ExpenseFilter = () => {
+
+  // const [filteredYear, setFilteredYear] = useState('');
+
+  const clickHandler = (event) => {
+    console.log(`Filtered Year Selected: ${event.target.value}`);
+  }
+
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select>
+        <select onChange={clickHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
