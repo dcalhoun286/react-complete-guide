6db4098cf36971f57b81a9a9cad3266eec4fcc9a
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-03-20 12:15:37 -0700

    ExpenseForm being rendered in NewExpense

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,12 @@
 import './NewExpense.css';
 
+import ExpenseForm from './ExpenseForm';
+
 function NewExpense () {
 
   return (
     <div className='new-expense'>
-
+      <ExpenseForm />
     </div>
   )
 }
