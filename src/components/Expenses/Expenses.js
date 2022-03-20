ab646a0a147b4b8981e5a7d8420513df498ad91b
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-03-20 13:28:41 -0700

    dynamically render ExpenseItem components by mapping over array

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,26 +8,14 @@ function Expenses (props) {
   return (
 
     <Card className='expenses'>
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
+      {props.expenses.map((el) => (
+        <ExpenseItem
+          key={el.id}
+          title={el.title}
+          amount={el.amount}
+          date={el.date}
+        />
+      ))}
     </Card>
 
   )
