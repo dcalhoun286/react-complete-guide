d8afb6889b9d8a12b0d77519aecc0873c64a4761
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-03-20 16:46:53 -0700

    don't need to manage state in this component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,9 @@
-import { useState } from 'react';
-
 import './ExpenseItem.css';
 
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
-
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    console.log('Clicked!!!');
-
-    setTitle('Updated!');
-    console.log(title);
-  };
 
   return (
 
@@ -24,7 +13,6 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
  
   );
