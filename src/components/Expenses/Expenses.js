119a68484fd992f1bf23cac1bdacbaa49b15a2ac
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-03-20 20:02:09 -0700

    removed logic outside of return

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -25,19 +25,25 @@ const Expenses = (props) => {
   expensesRendered = chosenYear ? filteredResults : expensesRendered;
   console.log('filtered results: ', filteredResults);
 
+  let expensesContent = <p>No expenses found.</p>
+
+  if (expensesRendered.length) {
+    expensesContent = expensesRendered.map((el) => (
+      <ExpenseItem
+        key={el.id}
+        title={el.title}
+        amount={el.amount}
+        date={el.date}
+      />
+    ));
+  }
+
   return (
 
     <>
       <Card className='expenses'>
         <ExpenseFilter selected={chosenYear} onFilteredYearSelected={filteredYearHandler}/>
-        {expensesRendered.map((el) => (
-          <ExpenseItem
-            key={el.id}
-            title={el.title}
-            amount={el.amount}
-            date={el.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </>
 
